bb3b64315a3f64228bc2fad0cf45646e54524607
unknown <jamoliddinisaboyev@gmail.com>
2024-01-18 21:58:14 +0500

    updated

--- src/containers/features/import.js
+++ src/containers/features/import.js
@@ -1,6 +1,6 @@
-import com1 from '../../asset/com1.png'
-import com3 from '../../asset/com3.png'
-import com2 from '../../asset/com2.png'
+import com1 from '../../asset/_com1.png'
+import com3 from '../../asset/_com3.png'
+import com2 from '../../asset/_com2.png'
 
 
 
